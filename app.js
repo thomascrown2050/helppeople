b9608b42e64b3c2eb9627718253791159584e8df
thomascrown2050 <120330817+thomascrown2050@users.noreply.github.com>
2022-12-11 10:26:44 -0800

    commit15

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(function (req, res, next) {
+  console.log('Time: %d', Date.now())
+  next()
+})
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
