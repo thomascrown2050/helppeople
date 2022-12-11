a82ef3bdb39630e6668c4fb6d2cffbf0c9874d61
thomascrown2050 <120330817+thomascrown2050@users.noreply.github.com>
2022-12-11 15:19:59 -0800

    c24

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET users listing. */
+router.get('/cool', function(req, res, next) {
+  res.send('you are so cool');
+});
+
+
 module.exports = router;
