5c1bc2caaf1451e80ec9874016789aecfc74676f
thomascrown2050 <120330817+thomascrown2050@users.noreply.github.com>
2022-12-11 10:43:44 -0800

    c21

--- app.js
+++ app.js
@@ -22,10 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use(function (req, res, next) {
-  console.log('Time: %d', Date.now())
+const myLogger = function (req, res, next) {
+  console.log('LOGGED')
   next()
-})
+}
+
+app.use(myLogger)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
