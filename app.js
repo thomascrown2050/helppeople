a1b4d8743ae8bdd23f09666b74d368bea1ef6656
thomascrown2050 <120330817+thomascrown2050@users.noreply.github.com>
2022-12-11 18:49:55 -0800

    c37

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+const requestTime = function (req, res, next) {
+  req.requestTime = Date.now()
+  next()
+};
+
+app.use(requestTime);
+
+app.get('/', (req, res) => {
+  let responseText = 'Hello World!<br>'
+  responseText += `<small>Requested at: ${req.requestTime}</small>`
+  res.send(responseText)
+});
+
+app.listen(3000);
+
 /*
 app.get("/", function (req, res) {
 
